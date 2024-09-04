8c868bef49528e3c9c0e6936735fcaea7df7744a
diyar-direk <diyardireki111@gmail.com>
2024-09-04 02:39:44 -0700

    category page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Category from "./pages/Category";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/category" element={<Category />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import Card from "../components/Card";
 import Videos from "../components/Videos";
+import NewsComponents from "../components/NewsComponents";
 let counter = 0;
 let intervlaValue;
 export default function Home() {
@@ -61,100 +62,10 @@ export default function Home() {
       </div>
 
       <div className="news">
-        <div className="category container">
-          <Link>title</Link>
-          <article className="flex">
-            <div className="important card">
-              <Link className="image-hover">
-                <img src={require("./1.jpg")} alt="1"></img>
-              </Link>
-              <div className="time flex">
-                <Link> category </Link>
-                <p className="time">1 min ago</p>
-              </div>
-              <Link>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
-                rem iure veniam optio modi quo fuga repudiandae rerum,
-                dignissimos doloribus, doloremque perferendis porro totam dicta
-                reiciendis saepe ducimus error beatae!
-              </Link>
-              <p>
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Fugiat, dolores, modi libero fuga consectetur voluptas
-                praesentium deleniti repellendus placeat itaque labore, dicta
-                nesciunt laboriosam ratione blanditiis. Non sequi corrupti
-                reiciendis!
-              </p>
-            </div>
-
-            <div className="sub">
-              <div className="center">
-                <Link className="image-hover">
-                  <img src={require("./1.jpg")} alt="1"></img>
-                </Link>
-                <div className="flex-1">
-                  <div className="time flex">
-                    <Link> category </Link>
-                    <p className="time">1 min ago</p>
-                  </div>
-                  <Link> category </Link>
-                </div>
-              </div>
-              <div className="center">
-                <Link className="image-hover">
-                  <img src={require("./1.jpg")} alt="1"></img>
-                </Link>
-                <div className="flex-1">
-                  <div className="time flex">
-                    <Link> category </Link>
-                    <p className="time">1 min ago</p>
-                  </div>
-                  <Link> category </Link>
-                </div>
-              </div>
-              <div className="center">
-                <Link className="image-hover">
-                  <img src={require("./1.jpg")} alt="1"></img>
-                </Link>
-                <div className="flex-1">
-                  <div className="time flex">
-                    <Link> category </Link>
-                    <p className="time">1 min ago</p>
-                  </div>
-                  <Link> category </Link>
-                </div>
-              </div>
-              <div className="center">
-                <Link className="image-hover">
-                  <img src={require("./1.jpg")} alt="1"></img>
-                </Link>
-                <div className="flex-1">
-                  <div className="time flex">
-                    <Link> category </Link>
-                    <p className="time">1 min ago</p>
-                  </div>
-                  <Link> category </Link>
-                </div>
-              </div>
-              <div className="center">
-                <Link className="image-hover">
-                  <img src={require("./1.jpg")} alt="1"></img>
-                </Link>
-                <div className="flex-1">
-                  <div className="time flex">
-                    <Link> category </Link>
-                    <p className="time">1 min ago</p>
-                  </div>
-                  <Link> category </Link>
-                </div>
-              </div>
-              <Link className="all">see all link1</Link>
-            </div>
-          </article>
-        </div>
+        <NewsComponents title={true} />
         <Videos />
         <div className="category container">
-          <div className="grid-card">
+          <div className="flex-card">
             <Card />
             <Card />
             <Card />
@@ -170,18 +81,20 @@ intervalFun(counter);
 function intervalFun(counter) {
   intervlaValue = setInterval(() => {
     const divs = document.querySelectorAll("main > div.landing > div.w-100");
-    divs.forEach((ele) => {
-      ele.classList.remove("active");
-    });
-    divs[counter].classList.add("active");
-    const dots = document.querySelectorAll("main > div.landing .dots span");
-    dots.forEach((ele) => {
-      ele.classList.remove("active");
-    });
-    dots[counter].classList.add("active");
-    counter++;
-    if (counter === 3) {
-      counter = 0;
+    if (divs.length !== 0) {
+      divs.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+      divs[counter].classList.add("active");
+      const dots = document.querySelectorAll("main > div.landing .dots span");
+      dots.forEach((ele) => {
+        ele.classList.remove("active");
+      });
+      dots[counter].classList.add("active");
+      counter++;
+      if (counter === 3) {
+        counter = 0;
+      }
     }
   }, 10000);
 }
