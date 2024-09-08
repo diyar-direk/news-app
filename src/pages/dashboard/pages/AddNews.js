baac50e6bb6433728eff2cc032edc3daa77fbacf
diyar-direk <diyardireki111@gmail.com>
2024-09-07 23:33:00 -0700

    commit-3

--- src/pages/dashboard/pages/AddNews.js
+++ src/pages/dashboard/pages/AddNews.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./addnews.css";
+import { Context } from "../../../context/Context";
 
 const AddNews = () => {
-  function handleClick() {
+  function handleClick(e) {
+    e.stopPropagation();
     document
       .querySelector(
         "div.main .dashboard-container form.add-news .select-category"
       )
       .classList.toggle("active");
   }
+  const context = useContext(Context);
+  const dataType = context.dataType.map((e, index) => {
+    return (
+      <p key={index} data-type={e.toLowerCase()}>
+        {e}
+      </p>
+    );
+  });
+  document.body.addEventListener("click", () => {
+    const catDiv = document.querySelector(
+      "div.main .dashboard-container form.add-news .select-category.active"
+    );
+    if (document.body.contains(catDiv)) catDiv.classList.remove("active");
+  });
   return (
     <div className="main">
       <div className="dashboard-container center">
@@ -16,12 +32,7 @@ const AddNews = () => {
           <div className="no-wrap">
             <input className="flex-1" type="text" placeholder="category" />
             <i className="fa-solid fa-chevron-down" onClick={handleClick}></i>
-            <div className="select-category">
-              <p>Category</p>
-              <p>Category</p>
-              <p>Category</p>
-              <p>Category</p>
-            </div>
+            <div className="select-category">{dataType}</div>
           </div>
           <input type="text" placeholder="headline" />
           <input type="text" placeholder="summary" />
